8d4f343eadd9020204e0783eeada8cfd2b170d00
Harika <84726019+GantaHarika@users.noreply.github.com>
2021-06-16 11:25:31 +0530

    solutions

--- solns.c
+++ solns.c
@@ -1,2 +1,79 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int [],int);
+int min(int [],int);
+float average(int [],int);
+int mode(int [],int);
+int factors(int,int[]);
+
+// max function
+int max(int arr[], int count){
+  int max = arr[0];
+  for(int i=1; i<count; i++){
+    if(arr[i]> max) {
+      max = arr[i];
+    }
+  }
+  return max;
+       }
+// min function
+     int min( int arr[], int count){
+  int min = arr[0];
+  for(int i=1; i<count; i++){
+    if(arr[i]<min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+//average function
+float average(int arr[], int count){
+  float avg,sum = 0.0;
+  for(int i=0; i<count; i++){
+    sum += arr[i];
+  }
+  avg = sum/count;
+  return avg;
+}
+// mode function
+int mode(int arr[], int n){
+  int max=arr[0];
+  for(int i=1;i<n;i++) {
+    if(arr[i]>max) {
+      max = arr[i];
+    }
+  }
+  int len=max+1;
+  int counts[len] = {0};
+  for(int j=0;j<n;j++) {
+  counts[arr[j]]++;
+  }
+  int Max=counts[0]
+   for(int k=1;k<len;k++) {
+    if(counts[k]>Max) {
+      Max = counts[k];
+    }
+  }
+  return Max;
+  }
+//factors function
+int factors(int num, int arr[]){
+  int isPrime,count=0;
+for (int i=2;i<=num;i++) {
+     	if(num%i == 0){
+   			isPrime = 1;
+			for (int j=2;j<=i/2;j++) {
+				if(i % j == 0) {
+					isPrime = 0;
+					break;
+				}
+			} 
+			if(isPrime == 1)
+			{
+			   arr[count] = i;
+        count++;
+			}	          	
+		}
+   }
+  return arr[],count;
+}
